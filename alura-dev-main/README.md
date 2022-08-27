934e32e326ee6683dc26cfe70f69f9611f695ac1
Adriana Beatriz <93537941+driica@users.noreply.github.com>
2022-08-27 11:56:32 -0300

    Delete alura-dev-main directory

--- alura-dev-main/README.md
+++
@@ -1,2 +0,0 @@
-# alura-dev
-Alura Challenge
--- alura-dev-main/script.js
+++
@@ -1,9 +0,0 @@
-const colorSelector = document.querySelector('input[type="color"]');
-const backgroundCode = document.querySelector('.background-code');
-let inputColor;
-
-colorSelector.addEventListener('change', function(e) {
-    inputColor = e.target.value;
-    backgroundCode.style.background = inputColor;
-})
-
